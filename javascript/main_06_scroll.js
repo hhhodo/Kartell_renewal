a303a6c5abc4d487a964d4003f887387d7ffd13d
지은 <home212121@gmail.com>
2024-02-10 16:32:29 +0900

    dfdfs

--- javascript/main_06_scroll.js
+++ javascript/main_06_scroll.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
     var slides = $('.main_06_contents')
     var slide = $('.main_06_01_all')
-    var currentIdx = 0
+    var currentIdx = 0;
     var slideCount = slide.length;
-    var slideWidth = 0
+    var slideWidth = 0;
     var prevBtn = $('#main_06_prev')
     var nextBtn = $('#main_06_next')
 
-    updateWidth()
+
+    console.log(slide.css('width'))
 
     function updateWidth(){
-        var newSlideCount = slide.length;
+        var newSlideCount = slideCount;
         slideWidth = parseInt(slide.css('width'));
         
-        var newWidth = (slideWidth * newSlideCount) + 100;
-        console.log(slideWidth)
+        var newWidth = (slideWidth * newSlideCount);
         slides.css('width', newWidth + 'px');
+        slides.css('left', '0px')
     }
     // 버튼 클릭 시 슬라이드 애니메이션 실행
     nextBtn.on('click', function(){
@@ -25,10 +26,12 @@ $(document).ready(function(){
         moveSlide(currentIdx - 1);
     });
 
+    updateWidth()
+
     function moveSlide(num){
         slides.css('left', -num * slideWidth + 'px');
         currentIdx = num;
-        console.log(num)
+        console.log(slideWidth)
         if(currentIdx%4 === 0){
             slides.css('left', 0 + 'px');
             currentIdx = 0;
@@ -37,4 +40,13 @@ $(document).ready(function(){
             currentIdx = 3;
         }
     }
+    // 리사이즈 끝나고 0.3초마다 리셋
+    var delay = 300;
+    var timer = null; 
+    $(window).on('resize', function(){
+        clearTimeout(timer);
+        timer = setTimeout(function(){
+        document.location.reload();
+        }, delay);
+    });
 })
